92a31bcbb67a69f5df0ca475095a5e67215af3cd
Matthis Fradin <matthis.fradin2@gmail.com>
2026-09-13 19:36:02 +0200

    fix(data): synchroniser legacyFurnitureCategories avec l'etat actuel des meubles prod

--- src/data/legacyFurnitureCategories.js
+++ src/data/legacyFurnitureCategories.js
@@ -4,20 +4,16 @@ export const LEGACY_FURNITURE_CATEGORY_BY_ID = {
   adHBwlKyiudEjP9ChKaG: 'autre', // Ancienne credence
   IvIngpikD1u276Qiv1zo: 'chaise', // Tabouret industriel
   mHUsTLrBYvDbqXQ4HlH9: 'chaise', // Lot de 6 chaises espagnol
-  Yrq9LuSgEuYJUZaSIaTF: 'buffet', // Meuble de metier
   '3wDcI26xH3g4ZeVnjkib': 'buffet', // No1 Buffet Desserte
   b7yPiWDNrREsrZ7CyOaS: 'buffet', // No2 Buffet Desserte
   BGo9WUQ5i2ye7GWWEj6Z: 'table', // No30 Table de ferme
   '20yC5bioF0EofPenVvHC': 'table', // No36 table de jack
-  bhN8fMYAANLLNBKNUQBY: 'table', // No38 table de ferme fabrication artisanale
-  GhpesY9osftcuMmaD1lw: 'table', // Table monastere
   i6NBt9fdWxSPp4SOxxqM: 'chaise', // Paire de nourrice
   Evv6w9dEUuxkOlxuYEM8: 'chaise', // Tabouret vintage
   TUfnDGIKwTIoYrr1bSxI: 'chaise', // Tabouret pied tourne
   X6SzHhFrA9xHp7RWXDL2: 'chaise', // Tabouret annees 70 en hetre
   eZIFTz7uIWCbEUpkGuuk: 'chaise', // Tabouret demi-lune couleur beige
   kfdBLP7IaATSia7emTWX: 'chaise', // Tabouret demi-lune
-  pvSFfz4aCY14Nv1IaVuo: 'chaise', // Tabouret repose plante
   xTE8A4vZ7NiPdcSrHQoU: 'chaise', // Tabouret demi-lune pied boule
   '6ZJImkl8ehbbnJDYCOvk': 'autre', // Porte manteaux  vintage
   '9Rvxf72ouFn9OJ3UCagT': 'autre', // Grand tapis iranien
@@ -25,9 +21,8 @@ export const LEGACY_FURNITURE_CATEGORY_BY_ID = {
   iZ892KoIIgYThZ2Vjlpo: 'autre', // Boite en marqueterie ancienne avec sa clé
   lJnaCJL8gSRBwsnWWAkI: 'autre', // Lot de 2 miroirs bambou ancien
   rj7R4HQelgi8ZPTSoUsC: 'autre', // Lot de  7 flacons d'apothicaire couleur ambrée
-  yKyvXLZ2mwh2yAzb0ujq: 'autre', // Lampe Monix 1930
   A89ATxl7ULCgYpRIHZcI: 'autre', // Console en rotin
   vnEaQJ75pTTYwglvCll0: 'table', // Table exterieur sur mesures
   '2IhQRs5jXEUcdeFwy6rH': 'chaise', // Tabouret trépied torsadé
-  'MntNMErybmt47SgfNlJi': 'chaise', // Tabouret de berger rond-pied démontable
+  MntNMErybmt47SgfNlJi: 'chaise', // Tabouret de berger rond-pied démontable
 };
